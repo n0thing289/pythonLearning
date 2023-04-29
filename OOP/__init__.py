9d1770934d36ba25837f0d23b0c0c25b94bf3b33
n0thing289 <61171141+n0thing289@users.noreply.github.com>
2023-04-29 16:13:41 +0800

    oop

---
+++ OOP/__init__.py
@@ -0,0 +1,15 @@
+class A:
+    def __init__(self):
+        self.name = 1
+
+    def printSelf(self):
+        print(self.name)
+
+        """
+        
+        """
+        #TODO
+
+
+a = A()
+a.printSelf()
